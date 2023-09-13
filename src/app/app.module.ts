2ebcda508bf390cd213d5755a44d48a9c282eab3
camilakritzia <camilakritzia@yahoo.it>
2023-09-13 15:01:59 +0200

    aggiunta path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { CustomerLoginComponent } from './component/customer-login/customer-login.component';
+import { CustomerRegComponent } from './component/customer-reg/customer-reg.component';
+import { AdminDashComponent } from './component/customer-dash/admin-dash.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    CustomerRegComponent,
+    CustomerLoginComponent,
+    CustomerRegComponent
   ],
   imports: [
     BrowserModule,
