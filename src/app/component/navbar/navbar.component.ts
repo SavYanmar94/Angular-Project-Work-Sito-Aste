95cc6ed9f642c9d6bb00d95761f9dbf4b43c0bf3
gilovaglio <gilovaglio@gmail.com>
2023-09-14 18:02:45 +0200

    added one connections

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -10,10 +10,15 @@ export class NavbarComponent {
   //attributi
   @Input() position:any;
   @Output() userLoginFormPopup = new EventEmitter();
+  @Output() customerRegFormPopup = new EventEmitter();
 
   //attivazione popup form login user
   activateUserLoginFormPopup():void{
     this.userLoginFormPopup.emit();
+  }
+
+  activateCustomerRegFormPopup():void{
+    this.customerRegFormPopup.emit();
   }
 
 }
