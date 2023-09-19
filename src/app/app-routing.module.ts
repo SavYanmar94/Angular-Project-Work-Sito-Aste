4d36ebd88377f96f300ec9eee8c22b95d4731a0f
gilovaglio <gilovaglio@gmail.com>
2023-09-19 13:11:28 +0200

    added user guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { UserDashComponent } from './component/user-dash/user-dash.component';
 import { UserDashTwoComponent } from './component/user-dash-two/user-dash-two.component';
+import { USER_GUARD } from './auth/user-auth';
 
 const routes: Routes = [
   {path: "", component:HomeComponent},
-  {path: "user", component:UserDashComponent},
+  {path: "user", component:UserDashComponent, canActivate:[USER_GUARD]},
   {path: "**", redirectTo: ""}, //se l'url non esiste rimanda alla home
 ];
 
